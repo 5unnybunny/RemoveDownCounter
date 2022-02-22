4da3709524068667fe88aad1ea56db8480f76e33
Bunny_Cunny <97416657+bunnycunny@users.noreply.github.com>
2022-02-22 01:34:14 +0100

    Update hudteammate.lua

--- hudteammate.lua
+++ hudteammate.lua
@@ -1,5 +1,5 @@
 Hooks:PostHook(HUDTeammate, 'init', 'Remove_Default_Down_Counter_I_Hate_Forced_Overkill_Features',function(self)
 	self._panel:child("player"):child("revive_panel"):set_visible(false)
-	self._panel:child("callsign"):set_visible(true)
-	self._panel:child("callsign_bg"):set_visible(true)
+	self._panel:child("callsign"):set_visible(false)
+	self._panel:child("callsign_bg"):set_visible(false)
 end)
